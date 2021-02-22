742385f144fb1237ce1d9a67c97fe5427ca3260e
saramargarido <saramargarido@gmail.com>
2021-02-22 18:53:17 -0300

    counter

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -4,9 +4,15 @@ function App() {
   return (
     <div className="">
     <h1>Hello World</h1>
-    <Button color="red "/>
-    <Button color="blue" />
-    <Button color="green" />
+    <Button color="red">
+      Botão 1
+    </Button>
+    <Button color="blue">
+    Botão 2
+    </Button>
+    <Button color="green">
+    Botão 3
+    </Button>
     </div>
   );
 }
--- moveit/src/components/Button.tsx
+++ moveit/src/components/Button.tsx
@@ -1,11 +1,23 @@
+import { useState } from 'react'
 interface ButtonProps {
-    color: string
+    color: string,
+    children: string
 }
 
 export function Button(props: ButtonProps) {
+    const [counter, serCounter] = useState(1)
+
+    function increment() {
+        serCounter(counter + 1)    
+    }
+
     return (
-        <button type="button">
-            {props.color}
+        <button 
+            type="button" 
+            style={{ backgroundColor: props.color }}
+            onClick={increment}
+        >
+            {props.children} <strong>{counter}</strong>
         </button>
     )
 }
