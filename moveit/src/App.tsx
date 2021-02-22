3e7b0ece3acb130beaa4733abf5eea33f76edfca
saramargarido <saramargarido@gmail.com>
2021-02-22 20:42:25 -0300

    nlw-4 day 1

--- moveit/src/App.tsx
+++ moveit/src/App.tsx
@@ -1,18 +1,11 @@
-import { Button } from './components/Button'
+import { ExperienceBar } from './components/ExperienceBar'
+
+import './styles/global.css'
 
 function App() {
   return (
-    <div className="">
-    <h1>Hello World</h1>
-    <Button color="red">
-      Botão 1
-    </Button>
-    <Button color="blue">
-    Botão 2
-    </Button>
-    <Button color="green">
-    Botão 3
-    </Button>
+    <div className="container">
+      <ExperienceBar />
     </div>
   );
 }
