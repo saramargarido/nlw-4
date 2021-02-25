6150602f1fb8e24a6eb6a876de233844b8bd92dd
saramargarido <saramargarido@gmail.com>
2021-02-25 19:27:51 -0300

    contextos

--- moveit/moveit-next/src/contexts/ChallengesContext.tsx
+++ moveit/moveit-next/src/contexts/ChallengesContext.tsx
@@ -1,6 +1,15 @@
 import { createContext, useState, ReactNode } from 'react'
 
-export const ChallengesContext = createContext({})
+interface ChallengesContextData {
+    level: number,
+    currentExperience: number,
+    challengesCompleted: number,
+    levelUp: () => void
+    startNewChallenge: () => void
+}
+
+
+export const ChallengesContext = createContext({} as ChallengesContextData)
 
 interface ChallengesProviderProps {
     children:  ReactNode
@@ -8,12 +17,26 @@ interface ChallengesProviderProps {
 
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
     const [level, setLevel] = useState(1)
+    const [currentExperience, setCurrentExperience] = useState(0)
+    const [challengesCompleted, setChallengesCompleted] = useState(0)
   
     function levelUp () {
       setLevel(level + 1)
     }
+
+    function startNewChallenge() {
+        console.log('New Challenge')
+    }
+
     return (
-        <ChallengesContext.Provider value={{ level, levelUp }}>
+        <ChallengesContext.Provider 
+            value={{ 
+                level, 
+                currentExperience, 
+                challengesCompleted, 
+                levelUp,
+                startNewChallenge
+             }}>
             { children }
         </ChallengesContext.Provider>
     )
